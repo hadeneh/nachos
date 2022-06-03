d681a1a4801b46183f253039ec4fa2d75e572b47
Hadi <hadeneh@gmail.com>
2022-06-04 00:21:37 +0800

    quick updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Coding from './pages/Coding';
@@ -7,12 +7,14 @@ import MyBlockly from './pages/Blockly2'
 
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Navigate replace to="/home" />} />
-            <Route path='/home' element={<Home />}></Route>
-            <Route path='/coding' element={<Coding />}></Route>
-            <Route path='/blockly' element={<MyBlockly />}></Route>
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate replace to="/home" />} />
+                <Route path='/home' element={<Home />}></Route>
+                <Route path='/coding' element={<Coding />}></Route>
+                <Route path='/blockly' element={<MyBlockly />}></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
