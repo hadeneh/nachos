1309092ba05b95ad1b353e46e8dcceda7c9e2c3b
Hadi <hadeneh@gmail.com>
2022-06-06 18:43:59 +0800

    Cleaning up "Coding.jsx" after the transfer

--- src/pages/Coding.jsx
+++ src/pages/Coding.jsx
@@ -1,24 +1,13 @@
-// import React from 'react';
-// import { Link } from "react-router-dom"
-import Split from "react-split";
-
-// import MyBlockly from './MyBlockly/MyBlockly';
-import '../css/Coding.css';
-
-
-
-
-
 import React from 'react';
+import Split from "react-split";
+// import { Link } from "react-router-dom"
 
 import BlocklyJS from 'blockly/javascript';
 
-// import './MyBlockly/Blocks/customBlocks/customBlocks'
-// import './MyBlockly/Blocks/customBlocks/generator/generator'
-
-import BlocklyComponent, {Block} from './MyBlockly/BlocklyComponent'; // previously from "Coding.jsx" we had: import BlocklyComponent, { Block, Value, Field, Shadow } from './MyBlockly/BlocklyComponent';
-// import SampleBlock from './Blocks/sampleBlocks/sampleBlocks'
+import BlocklyComponent from './MyBlockly/BlocklyComponent'; // previously from "Coding.jsx" we had: import BlocklyComponent, { Block, Value, Field, Shadow } from './MyBlockly/BlocklyComponent';
 import { InitBlocks, MyBlocks } from './MyBlockly/Blocks/Blocks'
+
+import '../css/Coding.css';
 
 
 class MyBlockly extends React.Component {
@@ -68,39 +57,3 @@ class MyBlockly extends React.Component {
 
 export default MyBlockly;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function Coding() {
-//     return (
-//         <Split 
-// 			className="split" 
-// 			direction="horizontal" 
-// 			minSize={0} gutterSize={11} 
-// 			snapOffset={50} 
-// 			sizes={[57, 43]}
-// 		>
-//             <div className="left-box">
-//                 <MyBlockly />
-//             </div>
-
-//             <div className="right-box">
-//                 <h1>Click button to Download (output)</h1>
-//             </div>
-//         </Split>
-//     );
-// }
-
-// export default Coding;
